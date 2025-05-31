25563504fc46f938465dfb2d60de2f7d3afed360
Sandro Bohmer <sandro@bohmer.org>
2025-05-31 23:12:53 +0200

    Wahrscheinlichkeiten fix

--- src/main/java/me/sandrp/soulMagicSystem/Main.java
+++ src/main/java/me/sandrp/soulMagicSystem/Main.java
@@ -48,7 +48,7 @@ public final class Main extends JavaPlugin {
         databaseManager = new DatabaseManager();
         databaseManager.connect();
 
-        //cooldownManager = new CooldownManager(61000);
+        cooldownManager = new CooldownManager(61000);
         grabbedEntities = new HashMap<>();
 
         new GravityWandGrabTask().runTaskTimer(this, 0, 1);
--- src/main/java/me/sandrp/soulMagicSystem/customWeapons/ingredients/ChestListener.java
+++ src/main/java/me/sandrp/soulMagicSystem/customWeapons/ingredients/ChestListener.java
@@ -7,16 +7,13 @@ import org.bukkit.event.world.LootGenerateEvent;
 public class ChestListener implements Listener {
     @EventHandler
     public void onChestLoot(LootGenerateEvent event){
-        if(!event.getLootTable().getKey().getKey().contains("dungeon")){
-            return;
-        }
-        if(Math.random() < 0.03){
+        if(Math.random() < 0.005){
             event.getLoot().add(SoulCrystal.createItem(1));
         }
-        if(Math.random() < 0.05){
+        if(Math.random() < 0.008){
             event.getLoot().add(SoulCrystal.createItem(1));
         }
-        if(Math.random() < 0.01){
+        if(Math.random() < 0.002){
             event.getLoot().add(SoulCrystal.createItem(2));
         }
     }
